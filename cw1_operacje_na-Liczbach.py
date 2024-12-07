448fced89b38136befde825c5df7642a1c213a34
Monika_Mackiewicz <monika.el.mackiewicz@gmail.com>
2024-12-07 13:48:12 +0100

    Test

---
+++ cw1_operacje_na-Liczbach.py
@@ -0,0 +1,11 @@
+# dane od użytkownika
+
+number1 = float(input("Podaj pierwszą liczę "))
+number2 = float(input("Podaj drugą liczę "))
+
+#operacje matematyczne
+
+suma = number1 + number2
+potęgowanie = number1 ** number2
+
+print(f"Suma Twoich liczb {number1} i {number2} to {suma}")
